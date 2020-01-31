af2a33f8caf57e28e6083a4cb697995300281710
AsharImtiaz <ashar.imtiaz1992@gmail.com>
2020-01-31 23:59:16 +0100

    Enemy movement and rotation modification

--- Assets/enemyMovement.cs
+++ Assets/enemyMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class enemyMovement : MonoBehaviour
 {
+    public GameObject bigRobot;
+    Vector3 direction;
+    Vector3 newDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class enemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 5.0f);
+        //transform.Translate(Vector3.forward * Time.deltaTime * 5.0f);
+        direction = bigRobot.transform.position - transform.position;
+        float speed = 5.0f * Time.deltaTime;
+        newDirection = Vector3.RotateTowards(transform.forward, direction, speed, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+        transform.position = Vector3.MoveTowards(transform.position, bigRobot.transform.position, speed);
+        Destroy(gameObject, 7.0f);
     }
 }
