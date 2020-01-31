694280b2ca30e84676cb509ed5c1a942a63fd187
AsharImtiaz <ashar.imtiaz1992@gmail.com>
2020-01-31 22:47:03 +0100

    Enemy wave scene

---
+++ Assets/enemyInstantiate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyInstantiate : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    int maxWaittime;
+    int modifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxWaittime = 500;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        modifier = Random.Range(0, maxWaittime);
+        //Debug.Log(modifier);
+        if (modifier == maxWaittime - 1)
+        {
+            Instantiate(enemyPrefab, transform.position, transform.rotation);
+            Debug.Log("Enemy spawned");
+        }
+    }
+}
