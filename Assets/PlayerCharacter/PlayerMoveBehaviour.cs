46ed8631c47cf9f20dca184ed86b4940a7aa8696
Christopher <chris.wblack@live.de>
2020-01-31 22:02:33 +0100

    Added PlayerCharacterPrefab
    placeholder player character, placeholder bullet, bullet behaviour, movement and shooting behaviours

---
+++ Assets/PlayerCharacter/PlayerMoveBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveBehaviour : MonoBehaviour
+{
+    public Transform BottomHalf;
+    public float MoveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get movement direction
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.z = Input.GetAxis("Vertical");
+
+        if (moveDirection.sqrMagnitude > 0.5f)
+        {
+            moveDirection.Normalize();
+
+            // Move character
+            transform.position += moveDirection * Time.deltaTime * MoveSpeed;
+
+            // Rotate bottom half of character
+            float rotationAngle = Vector3.SignedAngle(BottomHalf.forward, moveDirection, BottomHalf.up);
+            BottomHalf.Rotate(BottomHalf.up, rotationAngle);
+        }
+    }
+}
---
+++ Assets/PlayerCharacter/PlayerShootBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootBehaviour : MonoBehaviour
+{
+    public Transform TopHalf;
+    public float SecBetweenShots;
+    public GameObject BulletPrefab;
+
+    private float SecToNextShot = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SecToNextShot = SecBetweenShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SecToNextShot > 0.0f)
+        {
+            SecToNextShot -= Time.deltaTime;
+        }
+
+        // Get shooting direction
+        Vector3 shootDirection = Vector3.zero;
+        shootDirection.x = Input.GetAxis("Horizontal2");
+        shootDirection.z = Input.GetAxis("Vertical2");
+
+        if (shootDirection.sqrMagnitude > 0.5f)
+        {
+            shootDirection.Normalize();
+
+            // Rotate top half of character
+            float rotationAngle = Vector3.SignedAngle(TopHalf.forward, shootDirection, TopHalf.up);
+            TopHalf.Rotate(TopHalf.up, rotationAngle);
+
+            if (SecToNextShot <= 0.0f)
+            {
+                Instantiate(BulletPrefab, TopHalf.position, TopHalf.rotation);
+                SecToNextShot += SecBetweenShots;
+            }
+        }
+    }
+}
