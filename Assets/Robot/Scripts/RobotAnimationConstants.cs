43ee2575f74669f3abe612cd34c1f8b7763cb1b7
OLianoudakis <gusliaor@student.gu.se>
2020-02-01 10:03:31 +0100

    new shader, roboV2, robotmat, robotanimator, robotcolliders

---
+++ Assets/Robot/Scripts/RobotAnimationConstants.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotAnimationConstants : MonoBehaviour
+{
+    public static string AnimationState = "AnimationState";
+    public static int AnimIdle = 0;
+    public static int AnimShoot = 1;
+    public static int AnimDamaged = 3;
+    public static string AnimDrill = "DrillInit";
+}
