f1cb8a59b1442ec4c7beed3ef11c1f80ccaf2af8
Christopher <chris.wblack@live.de>
2020-02-02 15:28:10 +0100

    Damage sound fix

--- Assets/Robot/Scripts/RobotBehaviour.cs
+++ Assets/Robot/Scripts/RobotBehaviour.cs
@@ -57,7 +57,10 @@ public class RobotBehaviour : MonoBehaviour, IDamageable
             audioSource.PlayOneShot(deacticatedClip, 1.0f);
         }
         healthPoints = Mathf.Clamp(healthPoints - damage, 0, MaxHealth);
-        audioSource.PlayOneShot(damageClip, 0.3f);
+        if (playClip)
+        {
+            audioSource.PlayOneShot(damageClip, 0.3f);
+        }
         UpdateHealthBar();
 
         if (healthPoints <= 0)
