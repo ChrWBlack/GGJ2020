35ead9f5e45894b6ad32f81764b84a732eeed59b
AsharImtiaz <ashar.imtiaz1992@gmail.com>
2020-02-02 12:50:59 +0100

    'optimized' bullet script

--- Assets/Bullets/NewBullets/Scripts/BulletBehaviour.cs
+++ Assets/Bullets/NewBullets/Scripts/BulletBehaviour.cs
@@ -11,7 +11,7 @@ public class BulletBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 3.0f);
     }
 
     // Update is called once per frame
